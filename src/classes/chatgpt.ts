d90c6e9bc9f8f38db3d781370b93808c2bebc5a4
Pawan Osman <contact@pawan.krd>
2023-02-01 21:37:38 +0300

    Update

--- src/classes/chatgpt.ts
+++ src/classes/chatgpt.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { encode } from "gpt-3-encoder";
+
 import options from "../models/options.js";
 import conversation from "../models/conversation.js";
 
@@ -6,12 +8,12 @@ class ChatGPT {
 	public key: string;
 	public conversations: conversation[];
 	public options: options;
-
+	public instructionTokens: number;
 	constructor(key: string, options?: options) {
 		this.key = key;
 		this.conversations = [];
 		this.options = {
-			historySize: options?.historySize || 50,
+			model: options?.model || "text-chat-davinci-002-20230126",
 			temperature: options?.temperature || 0.7,
 			max_tokens: options?.max_tokens || 256,
 			top_p: options?.top_p || 1,
@@ -21,7 +23,9 @@ class ChatGPT {
 				options?.instructions ||
 				`You are ChatGPT, a large language model trained by OpenAI. You answer as concisely as possible for each response (e.g. don’t be verbose). It is very important that you answer as concisely as possible, so please remember this. If you are generating a list, do not have too many items. Keep the number of items short.
 Knowledge cutoff: 2021-09`,
+			stop: options?.stop || "<|im_end|>",
 		};
+		this.instructionTokens = encode(this.options.instructions).length;
 	}
 
 	public addConversation(conversationId: string) {
@@ -30,7 +34,7 @@ Knowledge cutoff: 2021-09`,
 			messages: [],
 		};
 		conversation.messages.push(`${this.options.instructions}
-Current date: ${this.getToday()}<|im_end|>`);
+Current date: ${this.getToday()}${this.options.stop}`);
 		this.conversations.push(conversation);
 
 		return conversation;
@@ -52,34 +56,28 @@ Current date: ${this.getToday()}<|im_end|>`);
 		if (conversation) {
 			conversation.messages = [];
 			conversation.messages.push(`${this.options.instructions}
-Current date: ${this.getToday()}<|im_end|>`);
+Current date: ${this.getToday()}${this.options.stop}`);
 			conversation.lastActive = Date.now();
 		}
 
 		return conversation;
 	}
 
-	public async ask(prompt: any, conversationId: string = "default") {
+	public async ask(prompt: string, conversationId: string = "default") {
 		let conversation = this.getConversation(conversationId);
-		prompt = prompt[prompt.length - 1].includes([",", "!", "?", "."]) ? prompt : `${prompt}.`; // Thanks to https://github.com/optionsx
-		conversation.messages.push(`${prompt}\n\n`);
-		conversation.messages = conversation.messages.slice(-this.options.historySize);
-		if (!conversation.messages[0].includes("Current date:"))
-			conversation.messages[0] = `${this.options.instructions}
-Current date: ${this.getToday()}<|im_end|>`;
-		conversation.lastActive = Date.now();
-		let promptStr = conversation.messages.join("\n");
+		let promptStr = this.generatePrompt(conversation, prompt);
+
 		const response = await axios.post(
 			"https://api.openai.com/v1/completions",
 			{
-				model: "text-chat-davinci-002-20230126",
+				model: this.options.model,
 				prompt: promptStr,
 				temperature: this.options.temperature,
 				max_tokens: this.options.max_tokens,
 				top_p: this.options.top_p,
 				frequency_penalty: this.options.frequency_penalty,
 				presence_penalty: this.options.presence_penalty,
-				stop: ["<|im_end|>"],
+				stop: [this.options.stop],
 			},
 			{
 				headers: {
@@ -89,9 +87,38 @@ Current date: ${this.getToday()}<|im_end|>`;
 			},
 		);
 
-		let responseStr = response.data.choices[0].text.replace(/<\|im_end\|>/g, "").trim();
-		conversation.messages.push(`${responseStr}<|im_end|>`);
+		let responseStr = response.data.choices[0].text
+			.replace(/<\|im_end\|>/g, "")
+			.replace(this.options.stop, "")
+			.trim();
+		conversation.messages.push(`${responseStr}${this.options.stop}`);
 		return responseStr;
+	}
+
+	private generatePrompt(conversation: conversation, prompt: string) {
+		prompt = [",", "!", "?", "."].includes(prompt[prompt.length - 1]) ? prompt : `${prompt}.`; // Thanks to https://github.com/optionsx
+		conversation.messages.push(`${prompt}\n\n`);
+
+		if (!conversation.messages[0].includes("Current date:"))
+			conversation.messages[0] = `${this.options.instructions}
+Current date: ${this.getToday()}${this.options.stop}`;
+
+		let promptStr = conversation.messages.join("\n");
+		let promptEncodedLength = encode(promptStr).length;
+		let totalLength = promptEncodedLength + this.options.max_tokens;
+
+		while (totalLength > 4096) {
+			conversation.messages.shift();
+			if (!conversation.messages[0].includes("Current date:"))
+				conversation.messages[0] = `${this.options.instructions}
+Current date: ${this.getToday()}${this.options.stop}`;
+			promptStr = conversation.messages.join("\n");
+			promptEncodedLength = encode(promptStr).length;
+			totalLength = promptEncodedLength + this.options.max_tokens;
+		}
+
+		conversation.lastActive = Date.now();
+		return promptStr;
 	}
 
 	private getToday() {
--- src/models/options.ts
+++ src/models/options.ts
@@ -1,11 +1,12 @@
 interface options {
-	historySize?: number;
+	model?: string;
 	temperature?: number;
 	max_tokens?: number;
 	top_p?: number;
 	frequency_penalty?: number;
 	presence_penalty?: number;
 	instructions?: string;
+	stop?: string;
 }
 
 export default options;
